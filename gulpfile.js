cc30df432b37fb3c3c8618067b049799aa5f2bc6
Guilherme Diego <guidiego.expgames@gmail.com>
2016-11-02 16:58:13 -0200

    Improve environment and add first files

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 var gulp = require('gulp'),
     nano = require('gulp-cssnano'),
     sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
     handlebars = require('gulp-compile-handlebars');
 
+var root = process.cwd() + '/src/';
+
 var config = {
-    CSS_SRC: 'src/sass/main.sass',
+    CSS_SRC: root + 'sass/main.sass',
     CSS_AUTOPREFIXER: '',
-    CSS_DIST: 'dist/assets/style.css',
-    CSS_WATCH: 'src/sass/**/*',
-    HANDLEBARS_WATCH: 'src/templates/**/*',
-    HANDLEBARS_REDERED_TEMPLATES: 'src/templates/*.hbs',
+    CSS_DIST: 'dist/assets/',
+    CSS_WATCH: root + 'sass/**/*',
+    HANDLEBARS_WATCH: root + 'templates/**/*',
+    HANDLEBARS_REDERED_TEMPLATES: root + 'templates/*.hbs',
     HANDLEBARS_DIST: 'dist/',
     HANDLEBARS_CONFIG: {
-
+        batch : [root + 'templates/partials/']
     }
 }
 
@@ -27,7 +30,8 @@ gulp.task('css', function () {
 
 gulp.task('handlebars', function () {
     return gulp.src(config.HANDLEBARS_REDERED_TEMPLATES)
-                .pipe(handlebars(config.HANDLEBARS_CONFIG))
+                .pipe(handlebars({}, config.HANDLEBARS_CONFIG))
+                .pipe(rename({ extname: '.html' }))
                 .pipe(gulp.dest(config.HANDLEBARS_DIST))
 })
 
